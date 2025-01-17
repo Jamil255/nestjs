8169eabf606c89d771d53ec63611e108963aa811
unknown <jamilafzal255@gmail.com>
2025-01-17 06:08:03 +0500

    dto and class validator

--- src/song/song.controller.ts
+++ src/song/song.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { SongService } from './song.service';
+import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('song')
 export class SongController {
-    constructor(private songService: SongService){}
+  constructor(private songService: SongService) {}
   @Post()
-  create() {
-    return this.songService.create("b")
+  create(@Body() createSongDto: CreateSongDTO) {
+    return this.songService.create(createSongDto);
   }
   @Get()
   findAll() {
